f8fb0060138c93442a52212220a85c0a91f48d8a
fishg <fish@jianke.com>
2014-11-25 20:52:13 +0800

    支持制定host ip

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
 hostfile
 ========
 
-常用的的host，用来访问配合vpn加速google\twitter\facebook访问
+严格来讲这不是一个hosts文件,这是用来给极路由的hosts插件用的,支持通配符,比如: *.twitter.com  
+用来访问配合极路由的VPN智能模式,加速google\twitter\facebook访问.主要是通过以下两点改善速度:  
+1. 常用国外网站的DNS解析速度
+2. 如果有国内ip使用国内ip替换
 
-适合国内用。  
-1.屏蔽部分广告，如百度
-2.加速google部分资源载入（部分使用国内ip）
-3.update脚本最好在海外vps上运行获得新的new_hosts文件，本地运行确保dns走的vpn线路。
+适合国内极路由用户使用:  
+1.屏蔽部分广告，如部分百度推广  
+2.加速google部分资源载入（部分使用国内ip） 
+3.印象笔记更新使用国内CDN  
+
+ip更新方式:   
+
+``python update_hosts.py``
+
+脚本最好在海外vps上运行获得新的new_hosts文件，本地运行确保dns走的vpn线路,用自己使用的vpn获取ip理论上能得到你的vpn能最快访问的ip.
 
--- update_hosts.py
+++ update_hosts.py
@@ -4,22 +4,9 @@
 def getIp(domain):
     import socket
     domain = domain.strip("*.")
-    if domain.endswith(".adobe.com"):
-        print("%s:%s"%(domain,"127.0.0.1"))
-        return "127.0.0.1"
-    elif domain == "dl-ssl.google.com" or domain == "dl.google.com":
-        print("%s:%s"%(domain,"203.208.46.200"))
-        return "203.208.46.200"
-    elif domain == "jdk.dev":
-        print("%s:%s"%(domain,"192.168.1.8"))
-        return "192.168.1.8"
-    elif domain == "e7813.ca.s.tl88.net" or domain == "cdn1.evernote.com":
-        print("%s:%s"%(domain,"122.228.222.49"))
-        return "122.228.222.49"
-    else:
-        myaddr = socket.getaddrinfo(domain,'http')[0][4][0]
-        print("%s:%s"%(domain,myaddr))
-        return myaddr
+    myaddr = socket.getaddrinfo(domain,'http')[0][4][0]
+    print("%s:%s"%(domain,myaddr))
+    return myaddr
 
 if __name__ == "__main__":
     fp = open("hosts_template","r")
@@ -31,13 +18,17 @@ if __name__ == "__main__":
         line = line.strip()
         if not line.startswith("#") and line!="":
             try:
-                ip = getIp(line)
+                if " " in line:
+                    hosts += line + "\n"
+                else:
+                    ip = getIp(line)
+                    hosts += ip + " " + line + "\n"
             except Exception, e:
-                hosts += "127.0.0.1 " + line + "\n"
+                hosts += "0.0.0.0 " + line + "\n"
                 print("error:"+line)
                 print(e)
             else:
-                hosts += ip + " " + line + "\n"
+                pass
             finally:
                 pass
     fpw = open("new_hosts","w")
